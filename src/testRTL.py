af3828d94dfc732a1226e002899b60232d827c45
tom-hooper <thomas.john.hooper@gmail.com>
2020-12-04 09:27:09 +1100

    sample rate adjusted

--- src/testRTL.py
+++ src/testRTL.py
@@ -3,8 +3,8 @@ from rtlsdr import RtlSdr
 sdr = RtlSdr()
 
 # configure device
-sdr.sample_rate = 148912500  # Hz
-sdr.center_freq = 70e6     # Hz
+sdr.sample_rate = 12500  # Hz
+sdr.center_freq = 148912500     # Hz
 sdr.freq_correction = 10   # PPM
 sdr.gain = 'auto'
 
