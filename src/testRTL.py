c9fd8867272f9a9438150f418d7fc36abd31c042
tom-hooper <thomas.john.hooper@gmail.com>
2020-12-04 10:01:18 +1100

    new test

--- src/testRTL.py
+++ src/testRTL.py
@@ -1,12 +1,70 @@
-from rtlsdr import RtlSdr
+#!/usr/bin/python
 
-sdr = RtlSdr()
+'''
 
-# configure device
-sdr.sample_rate = 2.048e6  # Hz
-sdr.center_freq = 148912500     # Hz
-sdr.freq_correction = 10   # PPM
-sdr.gain = 'auto'
+!! This requires a recent build of Multimon-NG as the old builds wont accept a piped input !!
 
-print(sdr.read_samples(512))
+Change the rtl_fm string to suit your needs.. add -a POCSAG512 , 2400 etc if needed to the Multimon-ng string
+This just prints and writes to a file, you can put it in a threaded class and pass though a queue
+or whatever suits your needs.
+
+
+'''
+import time
+import sys
+import subprocess
+import os
+
+def curtime():
+    return time.strftime("%H:%M:%S %Y-%m-%d")
+
+with open('error.txt','a') as file:
+    file.write(('#' * 20) + '\n' + curtime() + '\n')
+
+multimon_ng = subprocess.Popen("rtl_fm -f 148.9125M -s 2.048e6 -g 0 -p 65 | multimon-ng /
+                                -a POCSAG512 -a POCSAG1200 -a POCSAG2400 /
+                                -f alpha -t raw -",
+                               #stdin=rtl_fm.stdout,
+                               stdout=subprocess.PIPE,
+                               stderr=open('error.txt','a'),
+                               shell=True)
+
+try:
+    while True:
+        line = multimon_ng.stdout.readline()
+        multimon_ng.poll()
+        if line.__contains__("Alpha:"):    # filter out only the alpha
+            if line.startswith('POCSAG'):
+                address = line[22:28].replace(" ", "").zfill(7)
+                message = line.split('Alpha:   ')[1].strip().rstrip('<ETB>').strip()
+                output=(address+' '+curtime()+' '+ message+'\n')
+                print address, curtime(), message
+                with open('pocsag.txt','a') as f:
+                    f.write(output)
+        if not "Alpha:" in line:
+            with open("missed.txt","a") as missed:
+                missed.write(line + '\n')
+
+except KeyboardInterrupt:
+    os.kill(multimon_ng.pid, 9)
+
+
+
+
+#from rtlsdr import RtlSdr
+#import asyncio
+
+#async def streaming():
+#    sdr = RtlSdr()
+#
+#    # configure device
+#    sdr.sample_rate = 2.048e6  # Hz
+#    sdr.center_freq = 148912500     # Hz
+#    sdr.freq_correction = 10   # PPM
+#    sdr.gain = 'auto'
+#    async for samplen in sdr.stream():
+#        multimon-ng -t raw -a POCSAG512 -a POCSAG1200 -a POCSAG2400 -a scope
+
+
+#print(sdr.read_samples(512))
 #test
